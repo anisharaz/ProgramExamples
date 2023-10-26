e269bf6db214f62d05691bfb2118b53cfc7b884e
Anish Araz <105302254+anisharaz@users.noreply.github.com>
2023-10-26 21:28:31 +0530

    Update renew_container_with_new_iamge.sh

--- renew_container_with_new_iamge.sh
+++ renew_container_with_new_iamge.sh
@@ -1,29 +1,37 @@
 #!/bin/env bash
 
-container="$1"
-image="$2"
+container="mysite"
+image="aaraz/primage:mysite_github"
 
 if [ ! "$container" ] || [ ! "$image" ] ;then
-  echo "$0 [container] [image]"
-  exit 0
+    echo "missing container name anf image in file"
+    exit 0
 fi
 
 if docker stop "$container" >>  /dev/null;then
-  echo "Stopped container $container successfully"
-  if docker container rm "$container" >> /dev/null;then
-    echo "Removed container $container successfully"
-    if docker run -d --name "$container" -p 8000:8000 "$image" >> /dev/null;then
-      echo "Created container $container successfully"
+    echo "Stopped container $container successfully"
+    if docker container rm "$container" >> /dev/null;then
+        echo "Removed container $container successfully"
+        if docker pull "$image" >>  /dev/null;then
+            echo "Pulled image $image successfully"
+            if docker run -d --name "$container" -p 80:80 "$image" >> /dev/null;then
+                echo "Created container $container successfully"
+            else
+                echo "Failed to create container $container"
+                exit 1
+            fi
+        else
+            echo "Failed to pull image $image"
+            exit 1
+        fi
     else
-      echo "Failed to create container $container"
-      exit 1
+        echo "Failed to remove container $container"
+        exit 1
     fi
-  else
-    echo "Failed to remove container $container"
-    exit 1
-  fi
 else
-  echo "Failed to stop container $container"
-  exit 1
+    # echo "Failed to stop container $container"
+    # exit 1
+    docker pull "$image"
+    docker run -d --name "$container" -p 80:80 "$image"
+    exit 0
 fi
-
